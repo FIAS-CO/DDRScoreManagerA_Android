439fa07c4e666321a9cff3e842e6ad78dd97b5d9
sig-ume <beyondthemoonface@gmail.com>
2025-04-19 19:21:57 +0900

    SDK35でEdge-to-Edgeが矯正になったことで新しい端末だと上下が見切れるようになったのでEtEを無効化

--- app/src/main/java/jp/linanfine/dsma/util/common/ActivitySetting.java
+++ app/src/main/java/jp/linanfine/dsma/util/common/ActivitySetting.java
@@ -3,8 +3,11 @@ package jp.linanfine.dsma.util.common;
 import jp.linanfine.dsma.util.file.FileReader;
 import jp.linanfine.dsma.value.AppearanceSettingsSp;
 import android.app.Activity;
+import android.os.Build;
 import android.view.View;
 import android.view.WindowManager;
+
+import androidx.core.view.WindowCompat;
 
 public class ActivitySetting {
 	public static void setFullScreen(Activity activity)
@@ -17,6 +20,14 @@ public class ActivitySetting {
 		else
 		{
 			activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		}
+
+		// SDK 35 以降 Edge-to-Edgeが強制になったのでステータスバー、ナビゲーションバーで見切れないよう修正
+		// 上のフルスクリーン設定のあとに実行しないと見切れが解消されないのでここでやってます
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+			activity.getWindow().setDecorFitsSystemWindows(true);
+		} else {
+			WindowCompat.setDecorFitsSystemWindows(activity.getWindow(), true);
 		}
 	}
 	public static void setTitleBarShown(Activity activity, View titleBarView)
